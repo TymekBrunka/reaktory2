6f1bffed61fc1dccb2a2e4d791c64e98ea507c26
TymekBrunka <tymkopolo@wp.pl>
2026-07-29 14:58:55 +0200

    working further

---
+++ deps/engine.cmake
@@ -0,0 +1,67 @@
+#math
+message(glm)
+CPMAddPackage(
+  NAME glm
+  VERSION 1.0.3
+  GITHUB_REPOSITORY g-truc/glm
+  GIT_TAG 1.0.3
+  OPTIONS
+    "GLM_ENABLE_CXX_20 ON"
+)
+
+# rendering
+message(glfw)
+CPMAddPackage(
+  NAME glfw3
+  VERSION 3.4
+  GITHUB_REPOSITORY glfw/glfw
+  GIT_TAG 3.4
+  OPTIONS
+    "GLFW_BUILD_EXAMPLES OFF"
+    "GLFW_BUILD_TESTS OFF"
+    "GLFW_BUILD_DOCS OFF"
+)
+
+message(glad)
+add_library(glad OBJECT deps/glad/src/gl.c)
+target_include_directories(glad PUBLIC deps/glad/include/)
+
+file(GLOB imgui_SRC
+  deps/imgui/*.hpp
+  deps/imgui/*.cpp
+)
+
+message(imgui)
+add_library(imgui OBJECT ${imgui_SRC})
+target_include_directories(imgui PUBLIC deps/imgui)
+target_compile_options(imgui PRIVATE "-DIMGUI_IMPL_OPENGL_LOADER_CUSTOM <glad/gl.h>")
+target_link_libraries(imgui glad glfw)
+
+message(imguizmo)
+CPMAddPackage(
+  NAME imguizmo
+  VERSION 1.90
+  GITHUB_REPOSITORY cedricguillemet/imguizmo
+  GIT_TAG 1.9
+)
+
+target_compile_features(imguizmo PRIVATE cxx_std_11)
+target_link_libraries(imguizmo PUBLIC imgui)
+
+#loading models
+message(assimp)
+CPMAddPackage(
+  NAME assimp
+  VERSION 6.0.4
+  GITHUB_REPOSITORY assimp/assimp
+  GIT_TAG v6.0.4
+  OPTIONS
+    "BUILD_SHARED_LIBS OFF"
+    "ASSIMP_BUILD_TESTS OFF"
+    "ASSIMP_INSTALL OFF"
+    "ASSIMP_BUILD_DOCS OFF"
+    "ASSIMP_BUILD_ZLIB OFF"
+    "ASSIMP_WARNINGS_AS_ERRORS OFF" #set but unused error causes assimp not to build
+    # "ZLIB_LIBRARY ${zlib_LIBRARIES}"
+    # "ZLIB_INCLUDE_DIR ${zlib_INCLUDE_DIRS}"
+)
--- deps/ext.cmake
+++ deps/ext.cmake
@@ -31,63 +31,20 @@ add_library(ZLIB::ZLIB ALIAS zlibstatic)
 
 get_target_property(zlibincludes zlibstatic INCLUDE_DIRECTORIES)
 set(ZLIB_INCLUDE_DIR ${zlibincludes} CACHE PATH "" FORCE)
-#
-set(OGRE_STATIC ON CACHE BOOL "" FORCE)
-set(OGRE_BUILD_SAMPLES OFF CACHE BOOL "" FORCE)
-set(OGRE_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-set(OGRE_BUILD_COMPONENT_OVERLAY_IMGUI ON CACHE BOOL "" FORCE)
-set(OGRE_BITES_STATIC_PLUGINS ON CACHE BOOL "" FORCE)
-set(OGRE_CONFIG_DOUBLE ON CACHE BOOL "" FORCE) 
- 
-CPMAddPackage( 
-  NAME Ogre3D 
-  VERSION 14.5.2 
-  GITHUB_REPOSITORY OGRECave/ogre
-  # GIT_TAG v14.5.2
-  GIT_TAG 4879225c39370278d362528d2b30f4170b91a656
-) 
- 
-message("ogre deps path: ${OGREDEPS_PATH}") 
- 
-# function(original_add_library) 
-#   _add_library(${ARGV})
-# endfunction()
-#
-# function(add_library target)
-#   # if(target IN_LIST UNWANTED_TARGETS)
-#   #   message(STATUS "Excluding library target: ${target}")
-#   #   return()  # Skip creating the target
-#   # endif()
-#   if (target STREQUAL "SDL3::SDL3")
-#     message("cnsdcnsdkjncdsc>>>>>>> sdl3 : ${ARGV2}")
-#   endif()
-#   if(target STREQUAL "SDL3::SDL3" AND ARGV2 STREQUAL "SDL3")
-#     return()  # Skip creating the target
-#   endif()
-#   original_add_library(${ARGV})  # Create the target if allowed
-# endfunction()
-#
 
-# set(THREADS_PREFER_PTHREAD_FLAG TRUE CACHE BOOL "" FORCE)
-# # find_package(Threads REQUIRED)
-# # message(${CMAKE_THREAD_LIBS_INIT})
-
-# file(GLOB imgui_SRC
-#   deps/imgui/*.h
-#   deps/imgui/*.cpp
-# )
-# add_library(
-#   imgui STATIC ${imgui_SRC}
-# )
-#
 # CPMAddPackage(
-#   NAME imguizmo
-#   GITHUB_REPOSITORY CedricGuillemet/ImGuizmo
-#   GIT_TAG 1.9
+#   NAME rpmalloc
+#   VERSION 2.0.0
+#   GITHUB_REPOSITORY mjansson/rpmalloc
+#   GIT_TAG 2.0.0
+#   DOWNLOAD_ONLY
 # )
-# target_link_libraries(imguizmo PUBLIC imgui)
+#
+# add_library(rpmalloc ${rpmalloc_SOURCE_DIR}/rpmalloc/rpmalloc.c)
+# target_include_directories(rpmalloc PUBLIC ${rpmalloc_SOURCE_DIR}/rpmalloc/)
 
 add_library(yyjson deps/yyjson/yyjson.c)
 target_include_directories(yyjson PUBLIC deps/yyjson/)
 
-# include(deps/excel.cmake)
+include(deps/engine.cmake)
+include(deps/excel.cmake)
